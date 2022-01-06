9f3fd1b4628b9e0dba849ef059e6952e6fcd431b
zaforkhan <71150804+zaforkhan@users.noreply.github.com>
2022-01-06 22:50:51 +0530

    content update

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["an entrepreneur", "a developer", "a business Consultant", "a freelancer", "a college going boy"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
